015aa68ff063f0348dea3f1314e84e39483cb6fe
Juan Manuel Delgado Marcos <juan.15121001@gmail.com>
2019-11-01 03:22:39 -0500

    link server

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,23 @@
 $(document).ready(() => {
-
-  $('#alertSuccess').hide();
-  $('#alertDanger').hide();
-  $('#iconMenu').on('click', () => {
-    $('#menu').toggleClass('active');
+  $("#alertSuccess").hide();
+  $("#alertDanger").hide();
+  $("#iconMenu").on("click", () => {
+    $("#menu").toggleClass("active");
   });
 });
 
-
 var btn = document.getElementById("btn");
 
-
-btn.addEventListener("click", enviar)
-
+btn.addEventListener("click", enviar);
 
 function enviar(e) {
-
   e.preventDefault();
-
 
   var name = document.getElementById("name");
   var lastName = document.getElementById("lastName");
   var dni = document.getElementById("dni");
   var email = document.getElementById("email");
   var universidad = document.getElementById("university");
-
 
   if (dni.value.length != 8 || dni.value.length == 0) {
     danger.setAttribute("style", "display:none");
@@ -34,34 +27,34 @@ function enviar(e) {
 
     return false;
   } else {
-    axios.post('http://localhost:3000/', {
-      name: name.value,
-      lastName: lastName.value,
-      dni: dni.value,
-      email: email.value,
-      universidad: universidad.value
-    }).then((res) => {
-      name.value = '';
-      lastName.value = '';
-      dni.value = '';
-      email.value = '';
-      universidad.value = '';
+    axios
+      .post("https://unamadserver.herokuapp.com/", {
+        name: name.value,
+        lastName: lastName.value,
+        dni: dni.value,
+        email: email.value,
+        universidad: universidad.value
+      })
+      .then(res => {
+        name.value = "";
+        lastName.value = "";
+        dni.value = "";
+        email.value = "";
+        universidad.value = "";
 
-      if (res.data.status) {
-        $('#alertSuccess').show();
-        $('#alertDanger').hide();
+        if (res.data.status) {
+          $("#alertSuccess").show();
+          $("#alertDanger").hide();
 
-        setTimeout(() => {
-          $('#alertSuccess').hide();
-        }, 2000);
-      } else {
-        $('#alertSuccess').hide();
-        $('#alertDanger').show();
-      }
-
-
-    }).catch((err) => {
-    });
+          setTimeout(() => {
+            $("#alertSuccess").hide();
+          }, 2000);
+        } else {
+          $("#alertSuccess").hide();
+          $("#alertDanger").show();
+        }
+      })
+      .catch(err => {});
   }
 
   return false;
